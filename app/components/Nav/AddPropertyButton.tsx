88889718e69473131b3602acb2edf501fc03c8a1
kimjonghak <ggipk@naver.com>
2025-01-29 12:19:35 +0900

    AirbnbClone - NavBar 만드는 중

---
+++ app/components/Nav/AddPropertyButton.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const AddPropertyButton = () => {
+  return <div>AddPropertyButton</div>;
+};
+export default AddPropertyButton;
---
+++ app/components/Nav/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import SearchFilters from "@/app/components/Nav/SearchFilters";
+import UserNav from "@/app/components/Nav/UserNav";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
+      <div className="max-w-[1500px] mx-auto px-6">
+        <div className="flex justify-between items-center">
+          <Link href="/public">
+            <Image src="/logo.png" alt="logo" width={180} height={38} />
+          </Link>
+
+          <div className="flex space-6 ">
+            <SearchFilters />
+          </div>
+          <div className="flex items-center space-x-6">
+            Add property
+            <UserNav />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
